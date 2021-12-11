fd0e4d67709dc2e2187475af18c54b2c212a2a8b
Shubham Rai <shubham.010793@gmail.com>
2021-12-11 21:44:36 +0530

    Added custom metrics

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"net/http"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "prom_go_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+
+func recordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
 
 func main() {
 	http.Handle("/metrics", promhttp.Handler())
